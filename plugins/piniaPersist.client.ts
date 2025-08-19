160f0a55040f2f245eb20e6292b1a0e3ea49c915
Minh Bui <btminh.dev@gmail.com>
2025-08-19 08:26:09 +0700

    cleanup nuxt repo

--- plugins/piniaPersist.client.ts
+++
@@ -1,8 +0,0 @@
-import type { Pinia } from "pinia";
-import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
-
-export default defineNuxtPlugin((nuxtApp) => {
-  // 👇 Cast $pinia to Pinia
-  const pinia = nuxtApp.$pinia as Pinia;
-  pinia.use(piniaPluginPersistedstate);
-});
